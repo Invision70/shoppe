f80ee49b479d23c24bd82092b21ef99389f736ca
Yuriy <Invision70@gmail.com>
2015-11-12 22:09:53 +0600

    move to settings: countries, states, tax rates, delivery services

--- lib/shoppe/default_navigation.rb
+++ lib/shoppe/default_navigation.rb
@@ -14,11 +14,7 @@ Shoppe::NavigationManager.build(:admin_primary) do
   add_item :products
   add_item :product_categories
   add_item :product_attributes
-  add_item :delivery_services
-  add_item :tax_rates
   add_item :users
-  add_item :countries
   add_item :newsletters
-  add_item :states
   add_item :settings
 end
