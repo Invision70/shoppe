1421a97114a34607c418b89e3fdc91578166946c
Yuriy <Invision70@gmail.com>
2015-11-20 21:35:08 +0600

    fixed promo codes (after OrderItem destroy)

--- app/models/shoppe/order_item/discount.rb
+++ app/models/shoppe/order_item/discount.rb
@@ -6,9 +6,12 @@ module Shoppe
     private
       # Refresh promo code if condition changed
       def refresh_promo_code
-        self.order.apply_promo_code(self.order.promo_code) if self.order.promo_code.present? && ! self.order.destroyed?
+        unless self.order.destroyed?
+          order_item = self.order.reload
+          order_item.apply_promo_code(order_item.promo_code) if order_item.promo_code.present?
+        end
       rescue Shoppe::Errors::InvalidPromoCode
-        self.order.clear_promo_code unless self.order.destroyed?
+        order_item.clear_promo_code unless order_item.destroyed?
       end
   end
 end
